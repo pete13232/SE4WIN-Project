85f5e640f4b113a365896ddf356d1868de5c111c
4Weerawat <weerawat190943@gmail.com>
2021-10-29 19:41:06 +0700

    Modify | edit users to user

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -3,13 +3,13 @@ import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 
 @Injectable()
-export class UsersService {
+export class UserService {
   create(createUserInput: CreateUserInput) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all users`;
+    return `This action returns all user`;
   }
 
   findOne(id: number) {
