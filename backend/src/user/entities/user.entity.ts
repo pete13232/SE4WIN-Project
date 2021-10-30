b60dd1ca41c2c14f2a204e6534af4b9b678a662d
4Weerawat <weerawat190943@gmail.com>
2021-10-30 13:18:51 +0700

    Create | user entity

--- backend/src/user/entities/user.entity.ts
+++ backend/src/user/entities/user.entity.ts
@@ -1,7 +1,35 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
 export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 500 })
+  username: string;
+
+  @Column({ length: 500 })
+  password: string;
+
+  @Column({ length: 500 })
+  firstname: string;
+
+  @Column({ length: 500 })
+  lastname: string;
+
+  @Column('text')
+  address: string;
+
+  @Column({ length: 10 })
+  phoneNumber: string;
+
+  @Column()
+  email: string;
+
+  @Column('int')
+  roleId: number;
+
+  @Column()
+  dateCreated: Date;
 }
