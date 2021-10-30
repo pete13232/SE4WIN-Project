9828738a7bbb8de6c2d2e107b338aeccd0688ceb
Krittayos Poomthong <krittayos.kp@mail.kmutt.ac.th>
2021-10-30 23:45:19 +0700

    Create | category entity

--- backend/src/category/entities/category.entity.ts
+++ backend/src/category/entities/category.entity.ts
@@ -1,7 +1,14 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 @ObjectType()
 export class Category {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn()
+  @Field(type => Int)
+  cateID: number;
+
+  @Column()
+  @Field()
+  cateName: string;
 }
