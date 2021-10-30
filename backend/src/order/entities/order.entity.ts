51604a9564ce64d2d10810f64b5df4c48d4e767f
Krittayos Poomthong <krittayos.kp@mail.kmutt.ac.th>
2021-10-31 00:09:30 +0700

    Create | order entity

--- backend/src/order/entities/order.entity.ts
+++ backend/src/order/entities/order.entity.ts
@@ -1,7 +1,46 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { type } from 'os';
+import { Column, Double, Entity, PrimaryGeneratedColumn, Timestamp } from 'typeorm';
 
+enum Order_Status {
+  awaiting,
+  pending,
+  success,
+  unsuccess
+}
+
+@Entity()
 @ObjectType()
 export class Order {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn()
+  @Field(type => Int)
+  orderID: number;
+
+  @Column()
+  @Field(type => Int)
+  userID: number;
+
+  @Column()
+  @Field(type => Int)
+  prodID: number;
+
+  @Column()
+  @Field(type => Int)
+  prodAmount: number;
+
+  @Column()
+  @Field()
+  orderTimestamp: Timestamp;
+
+  @Column()
+  @Field(type => Double)
+  orderPrice: number;
+
+  @Column()
+  @Field()
+  receiptURL: string;
+
+  @Column('int')
+  @Field(type => Int)
+  orderStatus: Order_Status;
 }
