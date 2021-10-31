d04ac7ec51865876686d74a5fb160e5d8205712c
Krittayos Poomthong <krittayos.kp@mail.kmutt.ac.th>
2021-10-31 16:10:51 +0700

    Update | Create category input

--- backend/src/category/dto/create-category.input.ts
+++ backend/src/category/dto/create-category.input.ts
@@ -2,6 +2,9 @@ import { InputType, Int, Field } from '@nestjs/graphql';
 
 @InputType()
 export class CreateCategoryInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int)
+  cateID: number;
+
+  @Field()
+  cateName: string;
 }
