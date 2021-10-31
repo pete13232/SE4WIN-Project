5b2c81be325c5f4f880b3f3f2b37c27b997f6b4e
Chawanrat Nurat <ibaart01@gmail.com>
2021-11-01 02:02:58 +0700

    Create | Route setup, Navbar trying

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Link, Route } from "react-router-dom";
+import NavbarBootstrap from "./components/NavbarBootstrap";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route path="/">
+        <NavbarBootstrap />
+      </Route>
     </div>
   );
 }
