723e4c5f5a0ed1f0fe33474774560f20fdde7b80
Chawanrat Nurat <ibaart01@gmail.com>
2021-11-02 08:26:19 +0700

    CREATE | Set up for graphql, route set for 3 pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,65 @@
+import { useState } from "react";
 import NavbarBootstrap from "./components/NavbarBootstrap";
 import Category from "./components/Category";
 import Product from "./components/Product";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
+import { Route, Link, Switch } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+/* ----------------- Graphql Setup ----------------------- */
+
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, locationn, path }) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: "http://localhost:5000/graphql " }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
+
+/* -------------------------------------------------------- */
 
 function App() {
+
   return (
     <div>
       <Container className="px-0" style={{ background: "#EAEAEA" }}>
-        <NavbarBootstrap />
-        <Category />
-        <Product />
-        <Footer />
+        <Switch>
+          <Route exact path="/">
+            <NavbarBootstrap themeStatus = {false}/>
+            <Category />
+            <Product />
+            <Footer />
+          </Route>
+          <Route exact path="/signup">
+            <NavbarBootstrap themeStatus = {true}/>
+            <h1>signup</h1>
+          </Route>
+          <Route exact path="/login">
+            <NavbarBootstrap themeStatus = {true}/>
+            <h1>login</h1>
+          </Route>
+          <Route path="/:id">
+            <h1>No page</h1>
+          </Route>
+        </Switch>
       </Container>
     </div>
   );
--- frontend/src/components/NavbarBootstrap.js
+++ frontend/src/components/NavbarBootstrap.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { useRouteMatch } from "react-router-dom";
 import {
   Navbar,
   Container,
@@ -7,18 +9,21 @@ import {
   Button,
 } from "react-bootstrap";
 
-const NavbarBootstrap = () => {
+const NavbarBootstrap = ({ themeStatus }) => {
   return (
-    <Navbar collapseOnSelect expand="lg" className="Navbar">
-      <Col md={2}>
-        <Navbar.Brand
-          href="#home"
-          className="px-3 d-flex justify-content-center"
-        >
-          <h1>FAPP</h1>
-        </Navbar.Brand>
-      </Col>
-      <Col md={7}>
+    <>
+      {themeStatus ? (
+        <Navbar collapseOnSelect expand="lg" className="NavbarB">
+          <Col md={2}>
+            <Navbar.Brand className="px-3 d-flex justify-content-center ">
+              <h1 style={{color: "#ff6600"}}>FAPP</h1>
+            </Navbar.Brand>
+          </Col>
+          <Col>
+            <div className="vl"></div>
+          </Col>
+
+          {/* <Col md={7}>
         <Form>
           <FormControl
             type="search"
@@ -26,31 +31,61 @@ const NavbarBootstrap = () => {
             className="me-2"
             aria-label="Search"
           />
-          {/* <Button variant="outline-success">Search</Button> */}
+          { <Button variant="outline-success">Search</Button> }
         </Form>
       </Col>
       <Col md={1} className="d-flex justify-content-center">
-        <a href="#">
+        <a href="/">
           <h2>Home</h2>
         </a>
       </Col>
       <Col md={1} className="d-flex justify-content-center">
-        <a href="#">
+        <a href="/signup" >
           <h2>Sign-up</h2>
         </a>
       </Col>
       <Col md={1} className="d-flex justify-content-center">
-        <a href="#">
+        <a href="/login">
           <h2>Log-in</h2>
         </a>
-      </Col>
-    </Navbar>
-
-    // <Container className="mx-0 test">
-    //     <h1>Pete</h1>
-    //     <h1>Pete</h1>
-    //     <h1>Pete</h1>
-    // </Container>
+      </Col> */}
+        </Navbar>
+      ) : (
+        <Navbar collapseOnSelect expand="lg" className="NavbarA">
+          <Col md={2}>
+            <Navbar.Brand className="px-3 d-flex justify-content-center">
+              <h1>FAPP</h1>
+            </Navbar.Brand>
+          </Col>
+          <Col md={7}>
+            <Form>
+              <FormControl
+                type="search"
+                placeholder="Search for product"
+                className="me-2"
+                aria-label="Search"
+              />
+              {/* <Button variant="outline-success">Search</Button> */}
+            </Form>
+          </Col>
+          <Col md={1} className="d-flex justify-content-center">
+            <a href="/">
+              <h2>Home</h2>
+            </a>
+          </Col>
+          <Col md={1} className="d-flex justify-content-center">
+            <a href="/signup">
+              <h2>Sign-up</h2>
+            </a>
+          </Col>
+          <Col md={1} className="d-flex justify-content-center">
+            <a href="/login">
+              <h2>Log-in</h2>
+            </a>
+          </Col>
+        </Navbar>
+      )}
+    </>
   );
 };
 
