6ddf83a3ffb2773abcaeb91f9ff2911499166678
Krittayos Poomthong <krittayos.kp@mail.kmutt.ac.th>
2021-11-05 12:58:40 +0700

    Update | Add module

--- backend/src/authen/authen.module.ts
+++ backend/src/authen/authen.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthenService } from './authen.service';
 import { AuthenResolver } from './authen.resolver';
 import { UserModule } from 'src/user/user.module';
+import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [UserModule],
-  providers: [AuthenResolver, AuthenService]
+  imports: [UserModule, PassportModule],
+  providers: [AuthenResolver, AuthenService, LocalStrategy]
 })
 export class AuthenModule {}
