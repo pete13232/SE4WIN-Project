f52c94780aed449468937cba1d6ee7d43f12616b
Krittayos Poomthong <krittayos.kp@mail.kmutt.ac.th>
2021-11-05 11:35:57 +0700

    Update | Add validateUser

--- backend/src/authen/authen.module.ts
+++ backend/src/authen/authen.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthenService } from './authen.service';
 import { AuthenResolver } from './authen.resolver';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [UserModule]
   providers: [AuthenResolver, AuthenService]
 })
 export class AuthenModule {}
--- backend/src/authen/authen.service.ts
+++ backend/src/authen/authen.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthenService {}
+export class AuthenService {
+    constructor(private userService: UserService){}
+
+    async validateUser(username: string, password: string): Promise<any> {
+        const user = await this.userService.findByUsername(username);
+
+        if(user && user.password == password){
+            const {username, password, ...rest} = user;
+            return rest;
+        }
+
+        return null;
+    }
+}
