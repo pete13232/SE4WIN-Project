a773f72a42ebe8a702b0d78789684a4672cc3b5b
Krittayos Poomthong <krittayos.kp@mail.kmutt.ac.th>
2021-11-05 12:45:24 +0700

    Update | Set login route

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,7 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Get, Injectable, Post, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './authen/local-auth.guard';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly appService: AppService){}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  login(): any{
+    return {}
+  }
+
+  @Get('secured')
   getHello(): string {
     return 'Hello World!';
   }
