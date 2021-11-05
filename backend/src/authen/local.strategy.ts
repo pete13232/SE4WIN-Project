fed43b908b6c43f7a15887251e92d38756e33363
Krittayos Poomthong <krittayos.kp@mail.kmutt.ac.th>
2021-11-05 12:46:15 +0700

    Update | Done local strategy

--- backend/src/authen/local.strategy.ts
+++ backend/src/authen/local.strategy.ts
@@ -1,3 +1,21 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy } from "passport-local";
+import { AuthenService } from "./authen.service";
 
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy){
+    constructor(private authenService: AuthenService){
+        super();
+    }
 
-export class LocalStrategy extends PassportStrategy{}
+    async validate(username: string, password: string): Promise<any>{
+        const user = await this.authenService.validateUser(username,password);
+
+        if(!user){
+            throw new UnauthorizedException();
+        }
+
+        return user;
+    }
+}
