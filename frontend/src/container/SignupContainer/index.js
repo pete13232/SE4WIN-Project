5010551b176345952090ae5b8249458fdb4a92d0
Chawanrat Nurat <ibaart01@gmail.com>
2021-11-07 03:42:50 +0700

    Update | Set up sign up form

--- frontend/src/container/SignupContainer/index.js
+++ frontend/src/container/SignupContainer/index.js
@@ -1,9 +1,45 @@
+import { useMutation } from "@apollo/client";
 import { Col, Row, Form, FloatingLabel, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import NavbarBootstrap from "../../components/NavbarBoostrap";
+import { CREATE_USER } from "../../Graphql/Mutations";
 import "./style.css";
 
 const SignupContainer = () => {
+
+  const [createUser, { error }] = useMutation(CREATE_USER)
+
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [firstname, setFirstname] = useState("")
+  const [lastname, setLastname] = useState("")
+  const [address, setAddress] = useState("")
+  const [phoneNumber, setPhoneNumber] = useState("")
+  const [email, setEmail] = useState("")
+
+
+  const addUser = () => {
+    
+    createUser({
+      variables: 
+      {
+          username:  username,
+          password: password,
+          firstname: firstname,
+          lastname: lastname,
+          address: address,
+          phoneNumber: phoneNumber,
+          email: email,
+      }
+    })
+    if (error){
+      console.log(error)
+    }
+  }
+
+
+
   return (
     <div>
       <NavbarBootstrap />
@@ -16,27 +52,51 @@ const SignupContainer = () => {
             <Form.Label className="fw-bold">Personal Information</Form.Label>
             <Form.Group className="mb-1" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control 
+              type="email" 
+              placeholder="Enter email" 
+              onChange = {(e) => {
+                setEmail(e.target.value)
+              }}/>
             </Form.Group>
-
             <Form.Group className="mb-1" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Enter password" />
+              <Form.Control 
+              type="password" 
+              placeholder="Enter password"
+              onChange = {(e) => {
+                setPassword(e.target.value)
+              }} />
             </Form.Group>
 
             <Form.Group className="mb-1" controlId="formBasicFirstname">
               <Form.Label>First name</Form.Label>
-              <Form.Control type="text" placeholder="Enter first name" />
+              <Form.Control 
+              type="text" 
+              placeholder="Enter first name" 
+              onChange = {(e) => {
+                setFirstname(e.target.value)
+              }}/>
             </Form.Group>
 
             <Form.Group className="mb-4" controlId="formBasicLastname">
               <Form.Label>Last name</Form.Label>
-              <Form.Control type="text" placeholder="Enter last name" />
+              <Form.Control 
+              type="text" 
+              placeholder="Enter last name"
+              onChange = {(e) => {
+                setLastname(e.target.value)
+              }} />
             </Form.Group>
             <Form.Label className="fw-bold">Contact Information</Form.Label>
             <Form.Group className="mb-1" controlId="formBasicEmail">
               <Form.Label>Phone</Form.Label>
-              <Form.Control type="tel" placeholder="Enter phone number" />
+              <Form.Control 
+              type="tel" 
+              placeholder="Enter phone number"
+              onChange = {(e) => {
+                setPhoneNumber(e.target.value)
+              }} />
             </Form.Group>
             <Form.Group controlId="formBasicEmail">
               <Form.Label>Address</Form.Label>
@@ -44,10 +104,13 @@ const SignupContainer = () => {
                 className="mb-1"
                 type="text"
                 placeholder="Enter address"
+                onChange = {(e) => {
+                  setAddress(e.target.value)
+                }}
               />
             </Form.Group>
 
-            <Button type="submit" className="mt-4">
+            <Button type="submit" className="mt-4" onClick = { addUser }>
               <h5>Sign-up</h5>
             </Button>
             <div className="alternative mt-2">
