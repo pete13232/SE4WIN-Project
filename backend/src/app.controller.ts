fbe4118e99834670db471bb501e00ec3118d706e
Krittayos Poomthong <krittayos.kp@mail.kmutt.ac.th>
2021-11-07 16:59:23 +0700

    Update | Corrected app controller

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './authen/local-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  login(@Request() req): any{
+    return req.user;
+  }
+
+  @Get('secured')
   getHello(): string {
     return this.appService.getHello();
   }
 }
+
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,17 +1,7 @@
-import { Get, Injectable, Post, Request, UseGuards } from '@nestjs/common';
-import { LocalAuthGuard } from './authen/local-auth.guard';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly appService: AppService){}
-
-  @UseGuards(LocalAuthGuard)
-  @Post('login')
-  login(@Request() req): any{
-    return req.user;
-  }
-
-  @Get('secured')
   getHello(): string {
     return 'Hello World!';
   }
