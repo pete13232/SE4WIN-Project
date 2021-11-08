1d4b0afcfc050ad43e273cb6fc03d674cf0796a8
4Weerawat <weerawat.fo@mail.kmutt.ac.th>
2021-11-08 23:45:38 +0700

    Delete | Old authen

--- backend/src/authen/authen.resolver.ts
+++
@@ -1,7 +0,0 @@
-import { Resolver } from '@nestjs/graphql';
-import { AuthenService } from './authen.service';
-
-@Resolver()
-export class AuthenResolver {
-  constructor(private readonly authenService: AuthenService) {}
-}
