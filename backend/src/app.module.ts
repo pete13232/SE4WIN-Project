9d5b1c5361d9321b595f7498dc60fe1785bf16a1
4Weerawat <weerawat.fo@mail.kmutt.ac.th>
2021-11-09 01:46:19 +0700

    Modify |Merge conflict

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
 import { OrderModule } from './order/order.module';
 import { ProductModule } from './product/product.module';
 import { CategoryModule } from './category/category.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import graphql_config from './config/graphql.config';
 import { EnvironmentConfigModule } from './config/environment-config/environment-config.module';
 import { TypeOrmConfigModule } from './config/typeorm.module';
 
