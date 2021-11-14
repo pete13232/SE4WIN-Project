121b600f0d0f56d66a36a91c8aac72b381966dd7
4Weerawat <weerawat.fo@mail.kmutt.ac.th>
2021-11-14 14:43:35 +0700

    Create | Container

---
+++ frontend/src/container/EditProfildContainer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavbarBootstrap from "../../components/NavbarBoostrap";
+
+const EditProfileContainer = () => {
+  return (
+    <div>
+      <NavbarBootstrap themeStatus={false} />
+      <h1>HelloWolrd</h1>
+    </div>
+  );
+};
+
+export default EditProfileContainer;
+
