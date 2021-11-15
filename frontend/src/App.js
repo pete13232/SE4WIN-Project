d25c8a86d9d6f7bab4d5b76783a97e18fd6955a6
Pete <peetawit.vong@mail.kmutt.ac.th>
2021-11-15 20:33:16 +0700

    Add | Profile page
    Waiting for edit function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import SignupContainer from "./container/SignupContainer/index.js";
 import LoginContainer from "./container/LoginContainer/index.js";
 import Footer from "./components/Footer/index.js";
 import ProductSelectContainer from "./container/ProductSelectContainer/index.js";
+import ProfileContainer from "./container/ProfileContainer/index.js";
 
 /* ----------------- Graphql Setup ----------------------- */
 
@@ -53,6 +54,9 @@ function App() {
           </Route>
           <Route path="/product">
             <ProductSelectContainer/>
+          </Route>
+          <Route path="/profile">
+            <ProfileContainer/>
           </Route>
           <Route path="/:id">
             <p>Page not found</p>
