a274011b9bb693ace67003423b73e2937d7b628f
4Weerawat <weerawat.fo@mail.kmutt.ac.th>
2021-11-29 11:55:19 +0700

    .

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(new ValidationPipe());
+  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
   // app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }));
   await app.listen(8000);
   // await app.listen(parseInt(process.env.APP_PORT));
